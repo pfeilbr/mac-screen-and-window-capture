b2aca02bcd8437d3ae0fad0698010b8fb10717a8
Brian <brian.pfeil@gmail.com>
2018-01-31 08:57:45 -0500

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# mac-screen-and-window-capture
+
+macOS app that demonstrates how to capture image/screenshot of a window and a
+screenshot of entire screen.
--- mac-screen-and-window-capture/ViewController.m
+++ mac-screen-and-window-capture/ViewController.m
@@ -12,9 +12,47 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    // Do any additional setup after loading the view.
+    [self capture];
 }
+
+- (void) capture {
+    
+    CFArrayRef windowList = CGWindowListCopyWindowInfo(kCGWindowListOptionOnScreenOnly, kCGNullWindowID);
+    NSArray *windows=(__bridge NSArray *)windowList;
+    
+    // capture image from every open window
+    for (NSDictionary *window in windows) {
+        NSNumber *windowNumber = (NSNumber*)[window valueForKey:(NSString*)kCGWindowNumber];
+        CGWindowID windowID = (CGWindowID)[windowNumber longValue];
+        NSDictionary *windowBounds = (NSDictionary*)[window valueForKey:(NSString*)kCGWindowBounds];
+        CGFloat windowX = [[windowBounds valueForKey:@"X"] floatValue];
+        CGFloat windowY = [[windowBounds valueForKey:@"Y"] floatValue];
+        CGFloat windowWidth = [[windowBounds valueForKey:@"Width"] floatValue];
+        CGFloat windowHeight = [[windowBounds valueForKey:@"Height"] floatValue];
+        
+        CGRect windowRect = CGRectMake(windowX, windowY, windowWidth, windowHeight);
+        CGImageRef windowImage = CGWindowListCreateImage(windowRect, kCGWindowListOptionIncludingWindow, windowID, kCGWindowImageDefault);
+        NSLog(@"breakpoint line");
+    }
+    
+    // capture image of entire screen
+    CGDirectDisplayID display = CGMainDisplayID();
+    CGImageRef cgImage = CGDisplayCreateImage(display);
+    CGFloat width = CGImageGetWidth(cgImage);
+    CGFloat height = CGImageGetHeight(cgImage);
+    NSImage *image = [[NSImage alloc] initWithCGImage:cgImage size:NSMakeSize(width, height)];
+    
+    NSURL *documentsURL = [[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask][0];
+    NSURL *fileURL = [documentsURL URLByAppendingPathComponent:@"screen.png"];
+    
+    NSData *tiffData = [image TIFFRepresentation];
+    id bitmap = [NSBitmapImageRep imageRepWithData:tiffData];
+    NSData *pngData = [bitmap representationUsingType:NSBitmapImageFileTypePNG properties:[NSDictionary dictionary]];
+    if (![pngData writeToURL:fileURL atomically:YES]) {
+        NSLog(@"failed to save");
+    }
+}
+
 
 
 - (void)setRepresentedObject:(id)representedObject {
